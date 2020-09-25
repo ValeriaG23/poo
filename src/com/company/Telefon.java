38a034cce23cf2f907da7ec27810bc202c29352a
ValeriaG23 <valeria.guranda@isa.utm.md>
2020-09-25 16:34:07 +0300

    crearea proiectului

---
+++ src/com/company/Telefon.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Telefon {
+    private String model;
+    private String culoare;
+    private int anul;
+    private String brend;
+
+    public Telefon(String model,String culoare,int anul,String brend)
+    {
+        this.model=model;
+
+    }
+}
